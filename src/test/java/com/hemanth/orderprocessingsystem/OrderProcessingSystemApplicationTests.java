221b17b846ead34c551f1a204aab027c2cfa65e2
Hemanth Dantu <hemanthdantu@Hemanths-MacBook-Air.local>
2026-06-06 18:36:16 -0500

    add Docker Compose PostgreSQL setup

--- src/test/java/com/hemanth/orderprocessingsystem/OrderProcessingSystemApplicationTests.java
+++ src/test/java/com/hemanth/orderprocessingsystem/OrderProcessingSystemApplicationTests.java
@@ -3,7 +3,12 @@ package com.hemanth.orderprocessingsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=" +
+                "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+                "org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration"
+})
 class OrderProcessingSystemApplicationTests {
 
     @Test
